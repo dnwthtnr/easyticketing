b13b1020bf893bcc9e06d46bbb9a9d95b23e03ea
dnwthtnr <dunworthtanner@gmail.com>
2024-03-13 22:06:12 -0400

    Update +page.server.ts
    (Committing from phone so no error check or ability to commit multiple files at once).Pulling url path to allow navigation bar to display current parent path

--- src/routes/navbar/+page.server.ts
+++ src/routes/navbar/+page.server.ts
@@ -2,6 +2,7 @@ import type { PageServerLoad } from '../$types.js';
 
 export const load = (async ({locals}) => {
     const user = await locals.user
+    const urlPath = await locals.url.path
     console.log('locals', locals)
-    return {'user': user}
+    return {'user': user, currentPath: urlPath}
 }) satisfies PageServerLoad;
